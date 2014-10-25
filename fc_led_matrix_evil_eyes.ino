f50ad5e040da56b7336f055e357fcb36bbf098ce
Art Dahm <art@dragongears.com>
2014-10-25 09:45:51 -0700

    Constant pupil Y position

--- fc_led_matrix_evil_eyes.ino
+++ fc_led_matrix_evil_eyes.ino
@@ -20,22 +20,19 @@
 #define LED_ON 1
 #define LED_OFF 0
 
-const int devCount = 2;
-int brightness = 15;
-const int ledPin = 13;
-
-LedControl lc=LedControl(12,11,10,devCount); 
-
 #define RIGHT_EYE 0
 #define LEFT_EYE  1
+
+// Initialize LED Controllers
+
+const int devCount = 2;
+int brightness = 15;
+
+LedControl lc=LedControl(12,11,10,devCount);
+
 uint8_t eyeMatrix[2][8];
 
-// Rather than assigning matrix addresses sequentially in a loop, each
-// has a spot in this array.  This makes it easier if you inadvertently
-// install one or more matrices in the wrong physical position --
-// re-order the addresses in this table and you can still refer to
-// matrices by index above, no other code or wiring needs to change.
-//static const uint8_t matrixAddr[] = { 0x70, 0x71 };
+// Eye bitmaps array
 
 static const uint8_t PROGMEM // Bitmaps are stored in program memory
 blinkImg[][8] = {    // Eye animation frames
@@ -126,73 +123,76 @@ blinkImg[][8] = {    // Eye animation frames
 #define EVIL_EYE_OFFSET (8 * 5)
 
 uint8_t
-blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }, // Blink bitmap sequence
+	blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }, // Blink bitmap sequence
     blinkCountdown = 100, // Countdown to next blink (in frames)
     gazeCountdown  =  75, // Countdown to next eye movement
     gazeFrames     =  50; // Duration of eye movement (smaller = faster)
 int8_t
-    eyeX = 3, eyeY = 4,   // Current eye position
-    newX = 3, newY = 4,   // Next eye position
-    dX   = 0, dY   = 0;   // Distance from prior to new position
+    eyeX = 3,   // Current eye position
+    newX = 3,   // Next eye position
+    dX   = 0;   // Distance from prior to new position
+
+#define EYE_Y 4
 
 
 void drawPixel(uint8_t *displaybuffer, int16_t x, int16_t y, uint8_t color) {
-  if ((y < 0) || (y >= 8)) return;
-  if ((x < 0) || (x >= 8)) return;
+	if ((y < 0) || (y >= 8)) return;
+	if ((x < 0) || (x >= 8)) return;
 
-  x = 7-x;
+	x = 7-x;
 
-  if (color) {
-    displaybuffer[y] |= 1 << x;
-  } else {
-    displaybuffer[y] &= ~(1 << x);
-  }
+	if (color) {
+		displaybuffer[y] |= 1 << x;
+	} else {
+		displaybuffer[y] &= ~(1 << x);
+	}
 }
 
 void drawVLine(uint8_t *displaybuffer, int16_t y, int16_t x, int16_t w, uint8_t color) {
-  for (int16_t i=x; i<x+w; i++) {
-    drawPixel(displaybuffer, i, y, color);
-  }
+	for (int16_t i=x; i<x+w; i++) {
+		drawPixel(displaybuffer, i, y, color);
+	}
 }
 
 void fillRect(uint8_t *displaybuffer, int16_t x, int16_t y, int16_t w, int16_t h, uint8_t color) {
-  for (int16_t i=y; i<y+h; i++) {
-     drawVLine(displaybuffer, i, x, w, color);
-  }
+	for (int16_t i=y; i<y+h; i++) {
+		drawVLine(displaybuffer, i, x, w, color);
+	}
 }
 
 void clear(uint8_t *displaybuffer) {
-  for (uint8_t i=0; i<8; i++) {
-    displaybuffer[i] = 0;
-  }
+	for (uint8_t i=0; i<8; i++) {
+		displaybuffer[i] = 0;
+	}
 }
 
 void drawBitmap(uint8_t *displaybuffer, const uint8_t *source) {
-  for (uint8_t i=0; i<8; i++) {
-    displaybuffer[i] = pgm_read_byte(&source[i]);
-  }
+	for (uint8_t i=0; i<8; i++) {
+		displaybuffer[i] = pgm_read_byte(&source[i]);
+	}
 }
 
 void writeDisplay(uint8_t *displaybuffer, uint8_t out) {
-  for (uint8_t i=0; i<8; i++) {
-    lc.setRow(out, i, displaybuffer[i]);
-  }
+	for (uint8_t i=0; i<8; i++) {
+		lc.setRow(out, i, displaybuffer[i]);
+	}
 }
 
 
 
 void setup() {
-    // Seed random number generator from an unused analog input:
-    randomSeed(analogRead(A0));
 
-  for(int address=0;address<devCount;address++) {
-    /*The MAX72XX is in power-saving mode on startup*/
-    lc.shutdown(address,false);
-    /* Set the brightness to a medium values */
-    lc.setIntensity(address,brightness);
-    /* and clear the display */
-    lc.clearDisplay(address);
-  }
+	// Seed random number generator from an unused analog input:
+	randomSeed(analogRead(A0));
+
+	for(int address=0;address<devCount;address++) {
+		/*The MAX72XX is in power-saving mode on startup*/
+		lc.shutdown(address,false);
+		/* Set the brightness to a medium values */
+		lc.setIntensity(address,brightness);
+		/* and clear the display */
+		lc.clearDisplay(address);
+	}
 }
 
 int blinkColorSwitchCounter = 0;
@@ -231,11 +231,8 @@ void loop()
     if (blinkColorSwitchCounter >= 5)
     {
         eyeX = 4;
-        eyeY = 4;
         newX = 4;
-        newY = 4;
         dX = 0;
-        dY = 0;
     }
 
     // Add a pupil (2x2 black square) atop the blinky eyeball bitmap.
@@ -245,26 +242,24 @@ void loop()
         fillRect(
                 &eyeMatrix[LEFT_EYE][0], 
                 newX - (dX * gazeCountdown / gazeFrames),
-                newY - (dY * gazeCountdown / gazeFrames),
+                EYE_Y,
                 2, 2, LED_OFF);
         fillRect(
                 &eyeMatrix[RIGHT_EYE][0], 
                 newX - (dX * gazeCountdown / gazeFrames) - 2,
-                newY - (dY * gazeCountdown / gazeFrames),
+                EYE_Y,
                 2, 2, LED_OFF);
         if(gazeCountdown == 0) {    // Last frame?
-            eyeX = newX; eyeY = newY; // Yes.  What's new is old, then...
-            newY = 4;
+            eyeX = newX; // Yes.  What's new is old, then...
             newX = random(3) + 3;
             dX            = newX - eyeX;             // Horizontal distance to move
-            dY            = newY - eyeY;             // Vertical distance to move
             gazeFrames    = random(3, 15);           // Duration of eye movement
             gazeCountdown = random(gazeFrames, 120); // Count to end of next movement
         }
     } else {
         // Not in motion yet -- draw pupil at current static position
-        fillRect(&eyeMatrix[LEFT_EYE][0], eyeX, eyeY, 2, 2, LED_OFF);
-        fillRect(&eyeMatrix[RIGHT_EYE][0], eyeX - 2, eyeY, 2, 2, LED_OFF);
+        fillRect(&eyeMatrix[LEFT_EYE][0], eyeX, EYE_Y, 2, 2, LED_OFF);
+        fillRect(&eyeMatrix[RIGHT_EYE][0], eyeX - 2, EYE_Y, 2, 2, LED_OFF);
     }
 
     // Refresh all of the matrices in one quick pass
