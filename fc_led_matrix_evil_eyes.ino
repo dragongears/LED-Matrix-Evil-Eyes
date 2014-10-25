c2d1d7a70ceae2d9a80ee1bb8e31c643eb028117
Art Dahm <art@dragongears.com>
2014-10-25 09:30:08 -0700

    Remove green eyes and motion detection code

--- fc_led_matrix_evil_eyes.ino
+++ fc_led_matrix_evil_eyes.ino
@@ -1,4 +1,3 @@
-// # vim:expandtab:shiftwidth=4:tabstop=4:smarttab:autoindent:autoindent:syntax=cpp
 // Halloween Eyes
 // Brian Enigma, <brian@netninja.com>
 // http://nja.me/eyes
@@ -21,19 +20,14 @@
 #define LED_ON 1
 #define LED_OFF 0
 
-#define LED_RED 1
-#define LED_YELLOW 2
-#define LED_GREEN 3
-
 const int devCount = 2;
 int brightness = 15;
 const int ledPin = 13;
 
 LedControl lc=LedControl(12,11,10,devCount); 
 
-#define MOTION_LED 13
-#define LEFT_EYE  1
 #define RIGHT_EYE 0
+#define LEFT_EYE  1
 uint8_t eyeMatrix[2][8];
 
 // Rather than assigning matrix addresses sequentially in a loop, each
@@ -45,48 +39,6 @@ uint8_t eyeMatrix[2][8];
 
 static const uint8_t PROGMEM // Bitmaps are stored in program memory
 blinkImg[][8] = {    // Eye animation frames
-    // The NICE (green) eye, both left and right
-    { B00111100,         // Fully open nice/green eye
-        B01111110,
-        B11111111,
-        B11111111,
-        B11111111,
-        B11111111,
-        B01111110,
-        B00111100 },
-    { B00000000,
-        B01111110,
-        B11111111,
-        B11111111,
-        B11111111,
-        B11111111,
-        B01111110,
-        B00111100 },
-    { B00000000,
-        B00000000,
-        B00111100,
-        B11111111,
-        B11111111,
-        B11111111,
-        B00111100,
-        B00000000 },
-    { B00000000,
-        B00000000,
-        B00000000,
-        B00111100,
-        B11111111,
-        B01111110,
-        B00011000,
-        B00000000 },
-    { B00000000,         // Fully closed nice/green eye
-        B00000000,
-        B00000000,
-        B00000000,
-        B10000001,
-        B01111110,
-        B00000000,
-        B00000000 },
-
     // The EVIL (red) eye, left
     { B00000000,         // Fully open evil/red eye
         B11000000,
@@ -179,8 +131,8 @@ blinkIndex[] = { 1, 2, 3, 4, 3, 2, 1 }, // Blink bitmap sequence
     gazeCountdown  =  75, // Countdown to next eye movement
     gazeFrames     =  50; // Duration of eye movement (smaller = faster)
 int8_t
-    eyeX = 3, eyeY = 3,   // Current eye position
-    newX = 3, newY = 3,   // Next eye position
+    eyeX = 3, eyeY = 4,   // Current eye position
+    newX = 3, newY = 4,   // Next eye position
     dX   = 0, dY   = 0;   // Distance from prior to new position
 
 
@@ -233,11 +185,6 @@ void setup() {
     // Seed random number generator from an unused analog input:
     randomSeed(analogRead(A0));
 
-//    pinMode(MOTION_LED, OUTPUT);
-//    digitalWrite(MOTION_LED, LOW);
-//    pinMode(A3, INPUT);
-//    digitalWrite(A3, LOW);
-
   for(int address=0;address<devCount;address++) {
     /*The MAX72XX is in power-saving mode on startup*/
     lc.shutdown(address,false);
@@ -246,12 +193,9 @@ void setup() {
     /* and clear the display */
     lc.clearDisplay(address);
   }
-
 }
 
 int blinkColorSwitchCounter = 0;
-int8_t eyeColor = LED_GREEN;
-int8_t eyeOffset = 0;
 
 void loop() 
 {
@@ -271,10 +215,9 @@ void loop()
             (blinkCountdown < sizeof(blinkIndex)) ? // Currently blinking?
             blinkIndex[blinkCountdown] :            // Yes, look up bitmap #
             0                                       // No, show bitmap 0
-            ][0] + eyeOffset;
+            ][0];
     drawBitmap(&eyeMatrix[LEFT_EYE][0], imageOffset);
-    if (LED_RED == eyeColor)
-        imageOffset += EVIL_EYE_OFFSET;
+    imageOffset += EVIL_EYE_OFFSET;
     drawBitmap(&eyeMatrix[RIGHT_EYE][0], imageOffset);
 
     // Decrement blink counter.  At end, set random time for next blink.
@@ -287,18 +230,12 @@ void loop()
     // Switch eye color mid-blink
     if (blinkColorSwitchCounter >= 5)
     {
-        eyeColor = (LED_GREEN == eyeColor) ? LED_RED : LED_GREEN;
-        eyeOffset = (LED_GREEN == eyeColor) ? 0 : EVIL_EYE_OFFSET;
-        blinkColorSwitchCounter = 0;
-        if (LED_RED == eyeColor)
-        {
-            eyeX = 4;
-            eyeY = 4;
-            newX = 4;
-            newY = 4;
-            dX = 0;
-            dY = 0;
-        }
+        eyeX = 4;
+        eyeY = 4;
+        newX = 4;
+        newY = 4;
+        dX = 0;
+        dY = 0;
     }
 
     // Add a pupil (2x2 black square) atop the blinky eyeball bitmap.
@@ -312,23 +249,13 @@ void loop()
                 2, 2, LED_OFF);
         fillRect(
                 &eyeMatrix[RIGHT_EYE][0], 
-                newX - (dX * gazeCountdown / gazeFrames) - (LED_RED == eyeColor ? 2 : 0),
+                newX - (dX * gazeCountdown / gazeFrames) - 2,
                 newY - (dY * gazeCountdown / gazeFrames),
                 2, 2, LED_OFF);
         if(gazeCountdown == 0) {    // Last frame?
             eyeX = newX; eyeY = newY; // Yes.  What's new is old, then...
-            if (LED_RED == eyeColor)
-            {
-                newY = 4;
-                newX = random(3) + 3;
-            }
-            else 
-            {
-                do { // Pick random positions until one is within the eye circle
-                    newX = random(7); newY = random(7);
-                    dX   = newX - 3;  dY   = newY - 3;
-                } while((dX * dX + dY * dY) >= 10);      // Thank you Pythagoras
-            }
+            newY = 4;
+            newX = random(3) + 3;
             dX            = newX - eyeX;             // Horizontal distance to move
             dY            = newY - eyeY;             // Vertical distance to move
             gazeFrames    = random(3, 15);           // Duration of eye movement
@@ -337,30 +264,12 @@ void loop()
     } else {
         // Not in motion yet -- draw pupil at current static position
         fillRect(&eyeMatrix[LEFT_EYE][0], eyeX, eyeY, 2, 2, LED_OFF);
-        fillRect(&eyeMatrix[RIGHT_EYE][0], eyeX - (LED_RED == eyeColor ? 2 : 0), eyeY, 2, 2, LED_OFF);
+        fillRect(&eyeMatrix[RIGHT_EYE][0], eyeX - 2, eyeY, 2, 2, LED_OFF);
     }
 
     // Refresh all of the matrices in one quick pass
     writeDisplay(&eyeMatrix[LEFT_EYE][0], LEFT_EYE);
     writeDisplay(&eyeMatrix[RIGHT_EYE][0], RIGHT_EYE);
 
-    // Check for motion. If motion, set to red eyes on the next blink.
-    // If still in motion, disallow it to switch back to green
-    // motion = HIGH == digitalRead(A3);
-    // digitalWrite(MOTION_LED, motion);
-    // if (motion)
-    if (0)
-    {
-        if (LED_GREEN == eyeColor)
-        {
-            if (blinkCountdown > 20)
-                blinkCountdown = 20;
-            blinkColorSwitchCounter = 4; // Switch on next blink
-        } else
-            blinkColorSwitchCounter = 0; // Hold off on switching
-    } else {
-        if (LED_GREEN == eyeColor)
-            blinkColorSwitchCounter = 0; // Hold off on switching
-    }
     delay(20); // ~50 FPS
 }
