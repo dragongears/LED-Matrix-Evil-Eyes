cdb73b718b25eed9c593ab29377ec91245af60ca
Art Dahm <art@dragongears.com>
2014-10-26 08:40:03 -0700

    Update description

--- fc_led_matrix_evil_eyes.ino
+++ fc_led_matrix_evil_eyes.ino
@@ -2,6 +2,11 @@
 // Art Dahm, <art@dahm.com>
 // http://dragongears.com
 //
+// Modified to use two LED matrix link boards and the LED matrix master from
+// FriedCircuits (http://friedcircuits.us/) instead of the original
+// Adafruit Bicolor LED Square Pixel Matrix with I2C Backpack although any
+// Arduino compatible board with MAX7219 LED Display Driver ICs should work
+//
 // Based on Halloween Eyes
 // Brian Enigma, <brian@netninja.com>
 // http://nja.me/eyes
